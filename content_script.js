09aa372886f1eb2ccb9db4ee49af87e54e0015ff
ashfun <ashbgrk@gmail.com>
2020-01-12 17:52:32 +0900

    screenshot

--- content_script.js
+++ content_script.js
@@ -1,3 +1,7 @@
 chrome.runtime.onMessage.addListener(function(msg) {
   $("body").css("background-color", msg.color);
+  /*chrome.tabs.captureVisibleTab(w.id, {format: 'png'}, function(url) {
+    console.log(url);
+  });
+  alert('hello!');*/
 });
--- popup.js
+++ popup.js
@@ -16,3 +16,15 @@ $("#red").on("click", () => {
     });
   });
 });
+
+$("#green").on("click", () => {
+  //送れない！！
+  // chrome.runtime.sendMessage({ color: "red" });
+
+  chrome.tabs.captureVisibleTab(null, {}, function (imageURI) {
+     //console.log(image); //the image variable is a base64 encoded image which you should be able to load in either canvas or src attribute of an image.
+     var url = imageURI.replace(/^data:image\/[^;]/, 'data:application/octet-stream')
+     window.open(url);
+  });
+});
+//https://www.codepool.biz/chrome-extension-to-save-web-page-screenshots-to-local-disk.html
