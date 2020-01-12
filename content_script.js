a67f6336563913a968cc61a04d3ca3310c796ab9
ashfun <ashbgrk@gmail.com>
2020-01-12 21:59:40 +0900

    improve

--- content_script.js
+++ content_script.js
@@ -11,20 +11,32 @@
 });*/
 
 isOn = false;
+borderColor = 'rgb(255, 215, 0)';
 
 chrome.runtime.onMessage.addListener(function(msg) {
   //$('body').html('<div id="screenshot_layer"></div>');
   //$('body').append('<div id="google_ext_screenshot_layer" style="background: none; width: 1440px; height: 2506px; z-index: 2147483647; position: absolute; top: 0px; left: 0px;"><canvas id="google_ext_screenshot_layer_canvas" width="1440" height="2506" style="background-color: position: absolute; top: 0; margin: 0px; padding: 0px; display: block;　pointer-events: none;"></canvas></div>');
-  var screenWidth = document.documentElement.scrollWidth;
-  var screenHeight = document.documentElement.scrollHeight;
-  $('body').append('<canvas id="google_ext_screenshot_layer_canvas" width="' + String(screenWidth) + '" height="'+ String(screenHeight) +'" style="pointer-events: none; background: none; z-index: 2147483647; position: absolute; top: 0; left: 0px; margin: 0px; padding: 0px; display: block;"></canvas>');
-  var canvas = document.getElementById("google_ext_screenshot_layer_canvas");
-  var ctx = canvas.getContext("2d");
-  //白でぬりつぶす
-  ctx.globalAlpha = 0.5;
-  ctx.fillStyle = "rgb(102, 102, 102)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  isOn = true;
+  if (msg.type === 'drawing') {
+    var screenWidth = document.documentElement.scrollWidth;
+    var screenHeight = document.documentElement.scrollHeight;
+    $('body').append('<canvas id="google_ext_screenshot_layer_canvas" width="' + String(screenWidth) + '" height="'+ String(screenHeight) +'" style="pointer-events: none; background: none; z-index: 2147483647; position: absolute; top: 0; left: 0px; margin: 0px; padding: 0px; display: block;"></canvas>');
+    var canvas = document.getElementById("google_ext_screenshot_layer_canvas");
+    var ctx = canvas.getContext("2d");
+    //白でぬりつぶす
+    ctx.globalAlpha = 0.5;
+    ctx.fillStyle = "rgb(102, 102, 102)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    isOn = true;
+  }
+  if (msg.type === 'color_change') {
+    if (msg.border_color === 'yellow') {
+      borderColor = 'rgb(255, 215, 0)';
+    } else if (msg.border_color === 'red') {
+      borderColor = 'rgb(220, 20, 60)'; //#dc143c
+    } else {
+      borderColor = 'rgb(0, 0, 205)'; //#0000cd
+    }
+  }
 });
 
 $('html').mousemove(function (e) {
@@ -50,7 +62,8 @@ $('html').mousemove(function (e) {
       // ブレンドモードの設定をもとに戻す。
       ctx.restore();
       ctx.globalAlpha = 1.0;
-      ctx.strokeStyle = 'rgb(255,215,0)';
+      //ctx.strokeStyle = 'rgb(255,215,0)';
+      ctx.strokeStyle = borderColor;
       ctx.lineWidth = 5;
       ctx.strokeRect(targetOffset.left,targetOffset.top,targetElement.outerWidth(),targetElement.outerHeight());
     });
--- popup.js
+++ popup.js
@@ -1,26 +1,13 @@
-$("#black").on("click", () => {
+$("#focus_mode").on("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
-      color: "black"
+      type: "drawing"
     });
   });
+  window.close();
 });
 
-$("#red").on("click", () => {
-  //送れない！！
-  // chrome.runtime.sendMessage({ color: "red" });
-
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      color: "red"
-    });
-  });
-});
-
-$("#green").on("click", () => {
-  //送れない！！
-  // chrome.runtime.sendMessage({ color: "red" });
-
+$("#screenshot").on("click", () => {
   chrome.tabs.captureVisibleTab(null, {}, function (imageURI) {
      //console.log(image); //the image variable is a base64 encoded image which you should be able to load in either canvas or src attribute of an image.
      var url = imageURI.replace(/^data:image\/[^;]/, 'data:application/octet-stream')
@@ -31,4 +18,27 @@ $("#green").on("click", () => {
      });
   });
 });
+
+var select = document.getElementById('border_color');
+select.onchange = function()
+{
+  // 選択されているoption要素を取得する
+  var selectedItem = this.options[ this.selectedIndex ];
+  //alert( selectedItem.value );
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+      type: "color_change",
+      border_color: selectedItem.value,
+    });
+  });
+  // $(".css-div").css("width", "100px").css("border", "#000 2px solid");
+  border_color = selectedItem.value;
+  if (selectedItem.value == 'yellow') {
+    $("#sample_drawing_target").css("border", "solid 5px #ffd700");
+  } else if (selectedItem.value == 'red') {
+    $("#sample_drawing_target").css("border", "solid 5px #dc143c");
+  } else {
+    $("#sample_drawing_target").css("border", "solid 5px #0000cd");
+  }
+}
 //https://www.codepool.biz/chrome-extension-to-save-web-page-screenshots-to-local-disk.html
