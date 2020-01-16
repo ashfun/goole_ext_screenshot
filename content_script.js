3ad995322d0c254e78fbbc12bf13b540127efc61
ashfun <ashbgrk@gmail.com>
2020-01-16 21:23:29 +0900

    format

--- content_script.js
+++ content_script.js
@@ -1,67 +1,75 @@
 isOn = false;
 borderColorSet = {
-  'yellow': 'rgb(255, 215, 0)',
-  'red': 'rgb(220, 20, 60)',
-  'blue': 'rgb(0, 0, 205)'
-}
-borderColor = borderColorSet['yellow'];
+  yellow: "rgb(255, 215, 0)",
+  red: "rgb(220, 20, 60)",
+  blue: "rgb(0, 0, 205)"
+};
+borderColor = borderColorSet["yellow"];
 
 chrome.runtime.onMessage.addListener(function(msg) {
-  //$('body').html('<div id="screenshot_layer"></div>');
-  //$('body').append('<div id="google_ext_screenshot_layer" style="background: none; width: 1440px; height: 2506px; z-index: 2147483647; position: absolute; top: 0px; left: 0px;"><canvas id="google_ext_screenshot_layer_canvas" width="1440" height="2506" style="background-color: position: absolute; top: 0; margin: 0px; padding: 0px; display: block;　pointer-events: none;"></canvas></div>');
-  if (msg.type === 'drawing') {
+  if (msg.type === "drawing") {
     if (document.getElementById("google_ext_screenshot_layer_canvas") == null) {
       var screenWidth = document.documentElement.scrollWidth;
       var screenHeight = document.documentElement.scrollHeight;
-      $('body').append('<canvas id="google_ext_screenshot_layer_canvas" width="' + String(screenWidth) + '" height="'+ String(screenHeight) +'" style="pointer-events: none; background: none; z-index: 2147483647; position: absolute; top: 0; left: 0px; margin: 0px; padding: 0px; display: block;"></canvas>');
+      $("body").append(
+        '<canvas id="google_ext_screenshot_layer_canvas" width="' +
+          String(screenWidth) +
+          '" height="' +
+          String(screenHeight) +
+          '" style="pointer-events: none; background: none; z-index: 2147483647; position: absolute; top: 0; left: 0px; margin: 0px; padding: 0px; display: block;"></canvas>'
+      );
     }
     borderColor = borderColorSet[msg.border_color];
     var canvas = document.getElementById("google_ext_screenshot_layer_canvas");
     var ctx = canvas.getContext("2d");
-    //白でぬりつぶす
     ctx.globalAlpha = 0.5;
     ctx.fillStyle = "rgb(102, 102, 102)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     isOn = true;
   }
-  if (msg.type === 'reset') {
+  if (msg.type === "reset") {
     var canvas = document.getElementById("google_ext_screenshot_layer_canvas");
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
   }
 });
 
-$('html').mousemove(function (e) {
+$("html").mousemove(function(e) {
   if (isOn) {
-    //var mouseX = e.pageX + 10;
-    //console.log(jQuery(":hover"));
-    jQuery(":hover").each(function () {
-       var targetElement = $(this);
-       var targetOffset = targetElement.offset();
-       var canvas = document.getElementById("google_ext_screenshot_layer_canvas");
-       var ctx = canvas.getContext("2d");
-       ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-       ctx.globalAlpha = 0.5;
-       ctx.fillStyle = "rgb(102, 102, 102)";
-       ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-       ctx.save();
-       ctx.globalCompositeOperation = 'destination-out';
-       ctx.globalAlpha = 1.0;
-       //ctx.strokeStyle = 'rgb(00,00,255)';
-       ctx.fillRect(targetOffset.left,targetOffset.top,targetElement.outerWidth(),targetElement.outerHeight());
-       //ctx.strokeRect(targetOffset.left,targetOffset.top,targetElement.outerWidth(),targetElement.outerHeight());
-      // ブレンドモードの設定をもとに戻す。
+    jQuery(":hover").each(function() {
+      var targetElement = $(this);
+      var targetOffset = targetElement.offset();
+      var canvas = document.getElementById(
+        "google_ext_screenshot_layer_canvas"
+      );
+      var ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+      ctx.globalAlpha = 0.5;
+      ctx.fillStyle = "rgb(102, 102, 102)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.save();
+      ctx.globalCompositeOperation = "destination-out";
+      ctx.globalAlpha = 1.0;
+      ctx.fillRect(
+        targetOffset.left,
+        targetOffset.top,
+        targetElement.outerWidth(),
+        targetElement.outerHeight()
+      );
       ctx.restore();
       ctx.globalAlpha = 1.0;
-      //ctx.strokeStyle = 'rgb(255,215,0)';
       ctx.strokeStyle = borderColor;
       ctx.lineWidth = 5;
-      ctx.strokeRect(targetOffset.left,targetOffset.top,targetElement.outerWidth(),targetElement.outerHeight());
+      ctx.strokeRect(
+        targetOffset.left,
+        targetOffset.top,
+        targetElement.outerWidth(),
+        targetElement.outerHeight()
+      );
     });
   }
 });
 
-$('html').click(function (e) {
+$("html").click(function(e) {
   isOn = false;
 });
--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 var cssBorderSet = {
-  'yellow': 'solid 5px #ffd700',
-  'red': 'solid 5px #dc143c',
-  'blue': 'solid 5px #0000cd'
+  yellow: "solid 5px #ffd700",
+  red: "solid 5px #dc143c",
+  blue: "solid 5px #0000cd"
 };
 $("#focus_mode").on("click", () => {
   if (localStorage.getItem("border_color") != null) {
     borderColor = localStorage.getItem("border_color");
   } else {
-    borderColor = 'yellow';
+    borderColor = "yellow";
   }
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {
@@ -27,48 +27,45 @@ $("#reset_mode").on("click", () => {
 });
 
 $("#screenshot").on("click", () => {
-  chrome.tabs.captureVisibleTab(null, {}, function (imageURI) {
-     //console.log(image); //the image variable is a base64 encoded image which you should be able to load in either canvas or src attribute of an image.
-     var url = imageURI.replace(/^data:image\/[^;]/, 'data:application/octet-stream')
-     //window.open(url);
-     //var test = location.href;
-     var test = 'screenshot';
-     var date = new Date();
-     var date_string = String(date.getTime());
-     chrome.tabs.query({"active": true}, function (tab) {
-         test = String(tab[0].url);
-         //alert(test);
-         //chrome.tabs.remove(tab[0].id); //切り替わったタブを削除
-         test = test.toLowerCase();
-         test = test.replace('/([a-z])+:\/\//', '');
-         test = test.substr(test.indexOf('://')+3);
-         result = test.replace(/[^a-z0-9]/g, '-');
-         chrome.downloads.download({
-           url: url,
-           filename: result + date_string + '.png'
-         });
-     });
+  chrome.tabs.captureVisibleTab(null, {}, function(imageURI) {
+    var url = imageURI.replace(
+      /^data:image\/[^;]/,
+      "data:application/octet-stream"
+    );
+    var file_name = "screenshot";
+    var date = new Date();
+    var date_string = String(date.getTime());
+    chrome.tabs.query({ active: true }, function(tab) {
+      file_name = String(tab[0].url);
+      file_name = file_name.toLowerCase();
+      file_name = file_name.replace("/([a-z])+:///", "");
+      file_name = file_name.substr(file_name.indexOf("://") + 3);
+      file_name = file_name.replace(/[^a-z0-9]/g, "-");
+      chrome.downloads.download({
+        url: url,
+        filename: file_name + date_string + ".png"
+      });
+    });
   });
 });
 
-var select = document.getElementById('border_color');
-select.onchange = function()
-{
-  // 選択されているoption要素を取得する
-  var selectedItem = this.options[ this.selectedIndex ];
+var select = document.getElementById("border_color");
+select.onchange = function() {
+  var selectedItem = this.options[this.selectedIndex];
   borderColor = selectedItem.value;
   localStorage.setItem("border_color", borderColor);
   $("#sample_drawing_target").css("border", cssBorderSet[borderColor]);
-}
-//https://www.codepool.biz/chrome-extension-to-save-web-page-screenshots-to-local-disk.html
+};
 
-$(function(){
-  function initialSetup(){
+$(function() {
+  function initialSetup() {
     if (localStorage.getItem("border_color") != null) {
-      $("#sample_drawing_target").css("border", cssBorderSet[localStorage.getItem("border_color")]);
+      $("#sample_drawing_target").css(
+        "border",
+        cssBorderSet[localStorage.getItem("border_color")]
+      );
       $("#border_color").val(localStorage.getItem("border_color"));
     }
-    //$("#text").val(localStorage.getItem("text"));
   }
   initialSetup();
 });
